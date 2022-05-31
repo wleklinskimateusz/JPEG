8599689c482dee53f9dd7ff00cba40c345356bf4
matix0508 <wleklinskimateusz05@gmail.com>
2022-05-31 17:54:00 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Setup
+install requirements with 
+```python
+pip install -r requirements.txt
+```
+if something doesn't work try running this command:
+```bash
+sudo apt-get install --reinstall libxcb-xinerama0
+```
---
+++ gui.py
@@ -0,0 +1,21 @@
+import sys
+from PyQt6 import QtCore, QtGui, QtWidgets, uic
+from PyQt6.QtCore import Qt
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.label = QtWidgets.QLabel()
+        canvas = QtGui.QPixmap(400, 300)
+        self.label.setPixmap(canvas)
+        self.setCentralWidget(self.label)
+        self.draw_something()
+
+    def draw_something(self):
+        painter = QtGui.QPainter(self.label.pixmap())
+        painter.drawLine(10, 10, 300, 200)
+        painter.end()
+
+
