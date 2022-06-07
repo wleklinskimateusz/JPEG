37fa0335019fb7684b2767044c667cd8da881c09
wleklinskimateusz05 <matix0508@gmail.com>
2022-06-07 21:50:17 +0200

    saved formatting on vsc

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from gui import Window, QApplication
 import sys
 
+
 def main():
     App = QApplication(sys.argv)
     window = Window()
     window.show()
     sys.exit(App.exec_())
 
- 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
