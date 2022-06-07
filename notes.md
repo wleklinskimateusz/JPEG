fc74d21b8fa7139c5bc19383889d864fb3034036
JTMalczewski <jasiek.malczewski@gmail.com>
2022-06-07 23:28:11 +0200

    add documentation and comments into code

---
+++ notes.md
@@ -0,0 +1,81 @@
+## Teory:
+
+##### According to the JPEG spec (ISO/IEC 10918-6:2013 (E), section 6.1):
+
+- Images encoded with only one component are assumed to be grayscale data in which 0 is black and 255 is white.
+- Images encoded with three components are assumed to be RGB data encoded as YCbCr unless the image contains an
+APP14 marker segment 
+- Images encoded with four components are assumed to be CMYK, with (0,0,0,0) indicating white unless the image 
+contains an APP14 marker segment 
+
+Most JPEG algorithm implementations use luminance and chrominance (YUV encoding) instead of RGB because human eye is pretty bad at seeing high-frequency brightness changes over a small area.
+
+Y - brightness of the color
+U - determine the color, amount of blue
+V - determine the color, amount of red
+
+##### JPEG encoding:
+- converts an image into chunks of 8x8 blocks of pixels called MCU
+- changes the range of values of the pixels so that they center on 0
+- applies Discrete Cosine Transformation to each block
+- uses quantization to compress the resulting block (and lose high frequency  data)
+- zig-zag process, to encode low frequency info first and put zeros to the end
+- Run-length, Delta and Huffman encoding
+
+##### DCT:
+take an 8x8 image block and tell us how to reproduce it using an 8x8 matrix of cosine functions. That tells us how much each cosine function (out of 64 total functions) contributes to the 8x8 input matrix.
+
+##### Quantization process:
+Divide the DCT coefficient matrix element-wise with the quantization matrix, round the result to an integer, and get the quantized matrix. It is important to note that because we apply quantization while decoding, we will have to make sure the colors fall in the [0,255] range. If they fall outside this range, we will have to manually clamp them to this range.
+
+##### Delta encoding:
+Delta encoding is a technique used to represent a byte relative to the byte before it.
+ [10 11 12 13 10 9] -> [10 1  2  3  0 -1]
+
+##### Run-length encoding
+Run-length encoding is a technique used to represent a number of bytes followed by repetitive value. DC (first value) in JPEG is encoded this way.
+
+##### Huffman encoding
+ It takes some input data, maps the most frequent characters to the smaller bit patterns and least frequent characters to larger bit patterns, and finally organizes the mapping into a binary tree
+
+##### Data storage:
+A JPEG contains up to 4 Huffman tables and these are stored in the “Define Huffman Table” section (starting with 0xffc4). The DCT coefficients are stored in 2 different Huffman tables. One contains only the DC values from the zig-zag tables and the other contains the AC values from the zig-zag tables. The DCT information for the luminance and chrominance channel is stored separately so we have 2 sets of DC and 2 sets of AC information giving us a total of 4 Huffman tables.
+
+In a greyscale image, we would have only 2 Huffman tables (1 for DC and 1 for AC) because we don’t care about the color.
+
+##### JPEG decoding
+- Extract the Huffman tables and decode the bits
+- Extract DCT coefficients by undoing the run-length and delta encodings
+- Use DCT coefficients to combine cosine waves and regenerate pixel values for each 8x8 block
+- Convert YCbCr to RGB for each pixel
+- Display the resulting RGB image
+
+##### we support only Baseline compression
+According to the standard, baseline will contain the series of 8x8 blocks right next to each other.
+
+
+## Decoding procedure
+
+##### DHT section contents:
+- Marker Identifier
+-- 0xff, 0xc4 to identify DHT marker
+-- 2 bytes
+- Length
+-- This specifies the length of Huffman table  
+-- 2 bytes
+- HT information, 
+-- bit 0..3: number of HT (0..3, otherwise error)
+-- bit 4: type of HT, 0 = DC table, 1 = AC table
+-- bit 5..7: not used, must be 0
+-- 1 byte
+- Number of Symbols
+-- Number of symbols with codes of length 1..16
+-- the sum(n) of these bytes is the total number of codes, which must be <= 256
+-- 16 bytes
+- Symbols
+-- Table containing the symbols in order of increasing code length
+-- n = total number of codes
+-- n bytes
+
+
+
--- project.py
+++ project.py
@@ -1,37 +1,5 @@
 from struct import unpack
 
-'''
-Teory:
-
-According to the JPEG spec (ISO/IEC 10918-6:2013 (E), section 6.1):
-
-Images encoded with only one component are assumed to be grayscale data in which 0 is black and 255 is white.
-
-Images encoded with three components are assumed to be RGB data encoded as YCbCr unless the image contains an
-APP14 marker segment as specified in 6.5.3, in which case the color encoding is considered either RGB or YCbCr
-according to the application data of the APP14 marker segment. The relationship between RGB and YCbCr is 
-defined as specified in Rec. ITU-T T.871 | ISO/IEC 10918-5.
-
-Images encoded with four components are assumed to be CMYK, with (0,0,0,0) indicating white unless the image 
-contains an APP14 marker segment as specified in 6.5.3, in which case the color encoding is considered either 
-CMYK or YCCK according to the application data of the APP14 marker segment. The relationship between CMYK and 
-YCCK is defined as specified in clause 7.
-
-Most JPEG algorithm implementations use luminance and chrominance (YUV encoding) instead of RGB 
-because human eye is pretty bad at seeing high-frequency brightness changes over a small area
-
-Y - brightness of the color
-U - determine the color, amount of blue
-V - determine the color, amount of red
-
-
-
-
-
-
-'''
-
-
 
 marker_mapping = {
     0xffd8: "Start of Image",
@@ -43,28 +11,105 @@ marker_mapping = {
     0xffd9: "End of Image"
 }
 
+class HuffmanTable:
+    def __init__(self):
+        self.root=[]
+        self.elements = []
+    
+    def BitsFromLengths(self, root, element, pos):
+        if isinstance(root,list):
+            if pos==0:
+                if len(root)<2:
+                    root.append(element)
+                    return True                
+                return False
+            for i in [0,1]:
+                if len(root) == i:
+                    root.append([])
+                if self.BitsFromLengths(root[i], element, pos-1) == True:
+                    return True
+        return False
+    
+    def GetHuffmanBits(self,  lengths, elements):
+        self.elements = elements
+        ii = 0
+        for i in range(len(lengths)):
+            for j in range(lengths[i]):
+                self.BitsFromLengths(self.root, elements[ii], i)
+                ii+=1
+
+    def Find(self,st):
+        r = self.root
+        while isinstance(r, list):
+            r=r[st.GetBit()]
+        return  r 
+
+    def GetCode(self, st):
+        while(True):
+            res = self.Find(st)
+            if res == 0:
+                return 0
+            elif ( res != -1):
+                return res
 
 class JPEG:
     def __init__(self, image_file):
         with open(image_file, 'rb') as f:               # opens image as binary file to read
             self.img_data = f.read()
     
+    def decodeHuffman(self, data):
+        '''
+        Data format:
+        [0] - information of the HT, header
+        [1:17] - information of the sum(n) bytes withe same amount of len(code number)
+        [17:n+17] - symbols in order of increasing code length
+
+        '''
+        offset = 0
+        header, = unpack("B",data[offset:offset+1])
+        offset += 1
+
+        
+        lengths = unpack("BBBBBBBBBBBBBBBB", data[offset:offset+16])     # Extract the 16 bytes containing length data
+        offset += 16
+
+        elements = []                                                    # Extract the elements after the initial 16 bytes
+        for i in lengths:
+            elements += (unpack("B"*i, data[offset:offset+i]))
+            offset += i 
+
+        print("Header: ",header)
+        print("lengths: ", lengths)
+        print("Elements: ", len(elements))
+
+        hf = HuffmanTable()                                             # generate custom HT
+        hf.GetHuffmanBits(lengths, elements)
+        data = data[offset:]                                            # pass leftover data
+
     def decode(self):
         data = self.img_data
         while(True):
-            marker, = unpack(">H", data[0:2])           # >H set set unpack function to big-endian data type (najbardziej znaczący bit z przodu) of unsigned short data type
-            print(marker_mapping.get(marker))           # 
-            if marker == 0xffd8:                        # start marker, process
-                data = data[2:]                         #
-            elif marker == 0xffd9:                      # end marker, process and if there is no more data break from the loop
-                return                                  #
-            elif marker == 0xffda:                      # scan marker, process with image data
+            marker, = unpack(">H", data[0:2])                           # >H set set unpack function to big-endian data type (najbardziej znaczący bit z przodu) of unsigned short data type
+            print(marker_mapping.get(marker))                           
+            if marker == 0xffd8:                                        # start marker, process
+                data = data[2:]                         
+            elif marker == 0xffd9:                                      # end marker, process and if there is no more data break from the loop
+                return                                  
+            elif marker == 0xffda:                                      # scan marker, process with image data
                 data = data[-2:]
             else:
-                lenchunk, = unpack(">H", data[2:4])
-                data = data[2+lenchunk:]            
+                len_chunk, = unpack(">H", data[2:4])
+                len_chunk += 2
+                chunk = data[4:len_chunk]
+
+                if marker == 0xffc4:
+                    self.decodeHuffman(chunk)
+                data = data[len_chunk:]            
             if len(data)==0:
-                break        
+                break       
+
+
+  
 
 if __name__ == "__main__":
     img = JPEG('profile.jpg')
